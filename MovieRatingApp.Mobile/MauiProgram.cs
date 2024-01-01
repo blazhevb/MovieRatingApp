696d1945550447ac4243177757594c389133cbc7
Borislav Blazhev <b.d.blazhev@gmail.com>
2024-01-01 20:30:19 +0200

    Add new font

--- MovieRatingApp.Mobile/MauiProgram.cs
+++ MovieRatingApp.Mobile/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Fast Food Icons.ttf", "FastFoodIcons");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
---
+++ MovieRatingApp.Mobile/Resources/FontsMapping/Icons.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieRatingApp.Mobile.Resources.FontsMapping
+{
+    public class Icons
+    {
+        public const string Popcorn = "\u0061";
+    }
+}
