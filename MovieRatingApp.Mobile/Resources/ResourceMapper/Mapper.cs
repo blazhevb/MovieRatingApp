dfa8511ccabd6efc60d0b002624a810988455e14
borislav.blazhev <borislav.blazhev@dais-software.com>
2024-01-02 18:50:46 +0200

    Add Images

---
+++ MovieRatingApp.Mobile/Resources/ResourceMapper/Mapper.cs
@@ -0,0 +1,13 @@
+﻿using MovieRatingApp.Mobile.Common.Enums;
+
+namespace MovieRatingApp.Mobile.Resources.ResourceMapper;
+
+
+public class Mapper
+{
+    public static Dictionary<EnumIndicatorImage, string> RatingIconName = new Dictionary<EnumIndicatorImage, string>()
+    {
+        { EnumIndicatorImage.Empty, "popcorn_empty.png" },
+        { EnumIndicatorImage.Full, "popcorn_full.png" }
+    };
+}
