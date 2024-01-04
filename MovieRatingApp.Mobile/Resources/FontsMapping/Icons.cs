48530ffa633a436714739efa783c57bda9b763bf
borislav.blazhev <borislav.blazhev@dais-software.com>
2024-01-04 16:09:45 +0200

    remove unnecessary files

--- MovieRatingApp.Mobile/Resources/FontsMapping/Icons.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieRatingApp.Mobile.Resources.FontsMapping
-{
-    public class Icons
-    {
-        public const string FONT_FAMILY = "Refreshbonus";
-        public const string EmptyIndicator = "p";
-        public const string FullIndicator = "q";
-    }
-}
