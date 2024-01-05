2602f5f7695e251ae25b6faceaf02c23f92e5658
Borislav Blazhev <b.d.blazhev@gmail.com>
2024-01-05 03:52:19 +0200

    Add simple logger

---
+++ MovieRatingApp.Mobile/AppLogger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRatingApp.Mobile
+{
+    public static class AppLogger
+    {
+        private static ILogger _logger;
+
+        public static void Initialize(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("AppLogger");
+        }
+
+        public static void LogError(Exception ex, string message)
+        {
+            _logger?.LogError(ex, message);
+        }
+    }
+}
